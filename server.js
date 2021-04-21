cd7be80a9dd52d930256a742d862805f8e988428
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2021-04-21 14:34:27 +0200

    remove unnecessary import

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const db = require('./db');
 
 const app = express();
 const testimonialsRoutes = require('./routes/testimonials.routes');
