d0290d7e38677d07efd324a6d47d6d2901359213
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2021-04-21 14:32:28 +0200

    Add seats routes

--- server.js
+++ server.js
@@ -1,68 +1,24 @@
 const express = require('express');
 const path = require('path');
-const { v4: uuidv4 } = require('uuid');
 const db = require('./db');
 
 const app = express();
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
-
+const seatsRoutes = require('./routes/seats.routes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
-
-
-app.get('/seats', (req,res) => {
-  res.json(db.seats);
-});
-
-app.get('/seats/:id', (req,res) => {
-  res.json(db.concerts.find(seat => seat.id == req.params.id));
-});
-
-app.post('/seats', (req,res) => {
-  const seat = {
-    id: uuidv4(),
-    day: req.body.day,
-    seat: req.body.seat,
-    client: req.body.client,
-    email: req.body.email
-  };
-  db.seats.push(seat);
-  return res.json(seat);
-});
-
-app.delete('/seats/:id', (req,res) => {
-  db.seats.forEach(seat => {
-    if(seat.id == req.params.id) {
-      const index = db.seats.indexOf(seat);
-      db.seats.splice(index,1);
-    }
-  })
-  return res.json(db.seats);
-});
-
-app.put('/seats/:id', (req,res) => {
-  db.seats.forEach(seat => {
-    if(seat.id == req.params.id){
-      seat.day = req.body.day,
-      seat.seat = req.body.seat,
-      seat.client = req.body.client,
-      seat.email = req.body.email
-    }
-  });
-  return res.json(db.seats);
-});
+app.use('/api', seatsRoutes);
 
 app.use((req,res) => {
-  return res.json({
+  return res.status(404).json({
     message: 'Not found...'
   });
 });
-
 
 app.listen('8000',() => {
   console.log(path.join(__dirname, 'Server is running on port 8000'));
