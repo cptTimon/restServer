64932e8f3fddb761696f2523fc3bb21f69a1b592
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2021-04-30 13:19:34 +0200

    nicely done

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -40,7 +40,7 @@ class SeatChooser extends React.Component {
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i+1) )}</div>}
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} /> }
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert> }
-        <h4>Seats taken: {(seats.filter(item => item.day === chosenDay)).length}/50</h4>
+        <h4>Free seats: {50 - (seats.filter(item => item.day === chosenDay)).length}/50</h4>
       </div>
     )
   };
