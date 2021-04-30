aa9d63caf20630921cdd9c49b9a06853222982f2
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2021-04-30 13:09:41 +0200

    Add counting taken seats

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -13,17 +13,16 @@ class SeatChooser extends React.Component {
     this.socket.on('seatsUpdated', seats => updateSeats(seats));
   }
 
-
   isTaken = (seatId) => {
     const { seats, chosenDay } = this.props;
 
     return (seats.some(item => (item.seat === seatId && item.day === chosenDay)));
   }
 
+
   prepareSeat = (seatId) => {
     const { chosenSeat, updateSeat } = this.props;
     const { isTaken } = this;
-
     if(seatId === chosenSeat) return <Button key={seatId} className="seats__seat" color="primary">{seatId}</Button>;
     else if(isTaken(seatId)) return <Button key={seatId} className="seats__seat" disabled color="secondary">{seatId}</Button>;
     else return <Button key={seatId} color="primary" className="seats__seat" outline onClick={(e) => updateSeat(e, seatId)}>{seatId}</Button>;
@@ -31,7 +30,7 @@ class SeatChooser extends React.Component {
   render() {
 
     const { prepareSeat } = this;
-    const { requests } = this.props;
+    const { requests, seats, chosenDay } = this.props;
 
     return (
       <div>
@@ -41,6 +40,7 @@ class SeatChooser extends React.Component {
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i+1) )}</div>}
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} /> }
         { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert> }
+        <h4>Seats taken: {(seats.filter(item => item.day === chosenDay)).length}/50</h4>
       </div>
     )
   };
