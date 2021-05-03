d9c364e4d696c3fa4bc2c64197367950fe08ef37
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2021-05-03 13:50:51 +0200

    change path

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -9,7 +9,7 @@ class SeatChooser extends React.Component {
   componentDidMount() {
     const { loadSeats, updateSeats } = this.props;
     loadSeats();
-    this.socket = io(process.env.NODE_ENV === 'production' ? 'localhost:8000' : 'localhost:3000');
+    this.socket = io(process.env.NODE_ENV === 'production' ? process.env.PUBLIC_URL : 'localhost:8000'); 
     this.socket.on('seatsUpdated', seats => updateSeats(seats));
   }
 
